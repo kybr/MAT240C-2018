9d89d7a4586dbba1f5e6e2a44777609a39f6f1a3
Karl Yerkes <karl.yerkes@gmail.com>
2018-11-14 16:16:09 -0800

    renaming + QuasiBandlimited example

---
+++ allo/phasors-with-offset-no-aliasing.cpp
@@ -0,0 +1,90 @@
+#include "al/core.hpp"
+#include "al/util/imgui/al_Imgui.hpp"
+using namespace al;
+
+#include "synths.h"
+using namespace diy;
+
+struct MyApp : App {
+  QuasiBandlimited osc1, osc2;
+  Line offset;
+  bool show_gui = true;
+  float background = 0.21;
+
+  void onCreate() override {
+    initIMGUI();
+    osc1.frequency(220);
+    osc2.frequency(220);
+    osc1.filter(0.707);
+    osc2.filter(0.707);
+  }
+
+  void onAnimate(double dt) override {
+    // pass show_gui for use_input param to turn off interactions
+    // when not showing gui
+    beginIMGUI_minimal(show_gui);
+    navControl().active(!imgui_is_using_input());
+  }
+
+  int operation = 0;
+
+  // 30~60 Hz
+  void onDraw(Graphics& g) override {
+    g.clear(background);
+    ImGui::SliderFloat("grayscale", &background, 0, 1);
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
+    static float offset_target = 0.2;
+    ImGui::SliderFloat("Phase Offset", &offset_target, 0, 1);
+    offset.set(offset_target, 0.05);
+
+    ImGui::SliderInt("Operation (+|-|*)", &operation, 0, 2);
+
+    // TODO: would love to SEE this waveform, graphically
+
+    endIMGUI_minimal(show_gui);
+  }
+
+  // 86 Hz
+  void onSound(AudioIOData& io) override {
+    while (io()) {
+      float f =
+          osc1.phase +
+          (2 * offset());  // 2 * because QuasiBandlimited phase goes(-1, 1)
+      if (f > 1) f -= 2;
+      osc2.phase = f;
+
+      float s = 0;
+      switch (operation) {
+        default:
+        case 0:
+          s = osc1.saw() + osc2.saw();
+          break;
+        case 1:
+          s = osc1.saw() - osc2.saw();
+          break;
+        case 2:
+          s = osc1.saw() * osc2.saw();
+          break;
+      }
+      s *= 0.1;
+      io.out(0) = s;
+      io.out(1) = s;
+    }
+  }
+
+  void onKeyDown(const Keyboard& k) override {
+    if (k.key() == 'g') {
+      show_gui = !show_gui;
+    }
+  }
+
+  void onExit() override { shutdownIMGUI(); }
+};
+
+int main() {
+  MyApp app;
+  app.initAudio(SAMPLE_RATE, BLOCK_SIZE, OUTPUT_CHANNELS, INPUT_CHANNELS);
+  app.start();
+}
